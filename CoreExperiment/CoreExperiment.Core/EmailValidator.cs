86606dcc9599dfc273bd839cc07272798f0fde7a
herougo <herougo@gmail.com>
2026-04-12 18:37:11 -0400

    [CORE] build(deps)|feat: added Moq library, created EmailValidator and started creating a test for it

---
+++ CoreExperiment/CoreExperiment.Core/EmailValidator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CoreExperiment.Core
+{
+    internal class EmailValidator
+    {
+        private readonly ICustomLogger _logger;
+
+        public EmailValidator(ICustomLogger logger)
+        {
+            _logger = logger;
+        }
+
+        public bool IsEmail(string text)
+        {
+            _logger.LogInfo("IsEmail");
+
+            try
+            {
+                MailAddress m = new MailAddress(text);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+    }
+}
--- CoreExperiment/CoreExperiment.Test/UnitTest1.cs
+++ CoreExperiment/CoreExperiment.Test/UnitTest1.cs
@@ -1,7 +1,16 @@
-﻿namespace CoreExperiment.Test
+﻿using CoreExperiment.Core;
+
+namespace CoreExperiment.Test
 {
     public class UnitTest1
     {
+        private readonly ICustomLogger _logger;
+
+        public UnitTest1()
+        {
+            _logger = new Mock<ICustomLogger>();
+        }
+
         [Fact]
         public void Test1()
         {
